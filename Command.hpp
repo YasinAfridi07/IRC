f93924e00decff1cd99ada6d6c5b760132da85ef
yusman <yasinusman222@gmail.com>
2024-09-27 19:43:05 +0400

    PRIVMSG Done, Time for tim to merge

--- Command.hpp
+++ Command.hpp
@@ -20,9 +20,11 @@ class Command{
 		~Command(void);
 
 		void join(std::string channel_name, std::string key_pass, User user_object); // Yasin
+		void privmsg(std::string receiver, const std::vector<std::string>& splitmsg, User user); // Yasin
 
 		std::vector<std::string> ft_split(std::string str, char delimiter);
-		std::vector<Channel>::iterator chan_exist(std::string channel);
+		std::vector<Channel>::iterator channel_exist(std::string channel);
+		std::vector<User>::iterator user_exist(std::string nick);
 };
 
 #endif
